65a2c5e51ccb7c2b11b119fa2d2d865f27d3f039
Kaceba <RobertoNinno23@outlook.it>
2022-11-15 18:04:17 +0100

    EF display model

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreUdemy.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace AspNetCoreUdemy.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
