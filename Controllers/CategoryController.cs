205e8584f2c25644d8e2fc0ca497e13a599c8534
Kaceba <RobertoNinno23@outlook.it>
2022-11-16 17:51:03 +0100

    added insert method

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -11,13 +11,38 @@ namespace AspNetCoreUdemy.Controllers
 
         public CategoryController(ApplicationDbContext db)
         {
-            this._db = db;   
+            this._db = db;
         }
 
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("NameEqualDOrder", "The Name can't match the Display Order");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
