667d1b356ff599ba2bebfbe8edc2efdb15aa5743
Kaceba <RobertoNinno23@outlook.it>
2022-11-29 18:06:33 +0100

    n tier architecture

---
+++ AspNetCoreUdemy.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreUdemy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreUdemy.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
