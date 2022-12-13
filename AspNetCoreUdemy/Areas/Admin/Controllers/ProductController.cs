c6d857241726c518684da616836e130013fe4724
Kaceba <RobertoNinno23@outlook.it>
2022-12-13 15:29:03 +0100

    functioning program

--- AspNetCoreUdemy/Areas/Admin/Controllers/ProductController.cs
+++ AspNetCoreUdemy/Areas/Admin/Controllers/ProductController.cs
@@ -12,10 +12,12 @@ namespace AspNetCoreUdemy.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnvironment;
 
-    public ProductController(IUnitOfWork unitOfWork)
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
     {
         this._unitOfWork = unitOfWork;
+        this._hostEnvironment = hostEnvironment;
     }
 
     public IActionResult Index()
@@ -63,14 +65,29 @@ public class ProductController : Controller
     //POST
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Upsert(ProductVM obj, IFormFile file)
+    public IActionResult Upsert(ProductVM obj, IFormFile? file)
     {
         if (ModelState.IsValid)
         {
-            //_unitOfWork.CoverType.Update(obj);
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            if (file != null) 
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"images\products");
+                var extension = Path.GetExtension(file.FileName);
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create)) 
+                {
+                    file.CopyTo(fileStreams);
+                }
+
+                obj.Product.ImageUrl= @"\images\products\" + fileName + extension;
+            }
+
+            _unitOfWork.Product.Add(obj.Product);
             _unitOfWork.Save();
 
-            TempData["Success"] = "CoverType edited successfully";
+            TempData["Success"] = "Product created successfully";
 
             return RedirectToAction("Index");
         }
